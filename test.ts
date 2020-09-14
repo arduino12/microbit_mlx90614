9b49e7b4278403a91beffb932ed3c0ca13ec1a1b
arduino12 <arduino12@users.noreply.github.com>
2020-09-14 07:15:36 +0300

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,7 +1,7 @@
 let temperature = 0
 basic.forever(() => {
-    temperature = MLX90614.temperature()
+    temperature = MLX90614.temperature(MLX90614_TEMPERATURE_ORIGIN.OBJECT)
     serial.writeNumber(temperature)
-    serial.writeLine("C.")
-    basic.pause(1000)
+    serial.writeLine("°C")
+    basic.pause(100)
 })
